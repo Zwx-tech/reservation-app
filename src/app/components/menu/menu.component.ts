396d2001fe523e388da20a6d3138542347ae6ee4
Zwx-tech <71787137+Zwx-tech@users.noreply.github.com>
2024-04-18 14:42:42 +0200

    extracted menu to separate component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
-
+  authService = inject(AuthService);
+  router = inject(Router);
 }
--- src/app/routes/landing-route/landing-route.component.ts
+++ src/app/routes/landing-route/landing-route.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-
+import { MenuComponent } from '../../components/menu/menu.component';
 @Component({
   selector: 'app-landing-route',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, MenuComponent],
   templateUrl: './landing-route.component.html',
   styleUrl: './landing-route.component.scss',
 })
